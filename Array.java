a9afb14cbe4efac3daf6c4dee1b800086b779ab0
Yadav <2460608@cognizant.com>
2025-12-02 17:21:06 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,17 @@
+public class Array
+{
+    public static void main(String[] args){
+
+        // Array initialization and creation
+        // dataType[] arr; (or) dataType []arr;, (or) datatype arr[];
+        // int nums[] = {2, 3, 5,6}; // Method 1
+
+        int nums[] = new int[4]; // It will create a size 4 array initialize defaul value 0;
+
+        // nums[1]=8;
+
+        System.out.println(nums[1]);
+
+
+    }
+}
