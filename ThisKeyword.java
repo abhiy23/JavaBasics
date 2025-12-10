8673de0d582f0bf20fb29caa6d91304773906b9a
Yadav <2460608@cognizant.com>
2025-12-10 17:47:32 +0530

    This keyword

--- ThisKeyword.java
+++ ThisKeyword.java
@@ -2,19 +2,39 @@ class Human
 {
     private int age;
     private String name;
+    
+    // public int getAge(){
+    //     return age;
+    // }
+    // public void setAge(int a){
+    //     age=a;
+    // }
 
-    public int getAge(){
+    // public String getName(){
+    //     return name;
+    // }
+    // public void setName(String n){
+    //     name=n;
+    // }
+
+    public int getAge() {
         return age;
     }
-    public void setAge(int a){
-        age=a;
+
+    // public void setAge(int age, Human obj){
+    //     Human obj1=obj; // here we are creating new object 
+    //     obj1.age=age; // here we want to change the value of instance variable
+    // }
+    public void setAge(int age) {
+        this.age = age;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public void setName(String n){
-        name=n;
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
 
@@ -26,7 +46,9 @@ public class ThisKeyword
         Human obj=new Human();
         // obj.age=11;
         // obj.name="Reddy";
-        obj.setAge(30);
+        // obj.setAge(30, obj);
+
+        obj.setAge(33);
         obj.setName("Reddy");
 
         System.out.println(obj.getName()+" : "+obj.getAge());
